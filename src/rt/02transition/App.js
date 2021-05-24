aa103b910d2fd40c5a5c434f7f30ef8415d220a3
Grigory Yakovlev <grigory.yakovlev96@gmail.com>
2021-05-24 17:47:29 +0300

    transform

--- src/rt/02transition/App.js
+++ src/rt/02transition/App.js
@@ -1,16 +1,18 @@
-import styles from "./App.module.css"
+import styles from "./App.module.css";
 import cn from "classnames";
 import {useState} from "react";
 
 export const App = () => {
-    const [animated, setAnimated] = useState(false)
+    const [animated, setAnimated] = useState(false);
 
     return (
-        <div style={{height: "100vh", width: "100vw"}}>
-            <button className={styles.button} onClick={() => setAnimated(!animated)}>toggle</button>
-            <div className={cn(styles.smile, {
-                [styles.isAnimated]: animated
-            })}/>
+        <div style={{height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <div className={styles.parent}>
+                <div onClick={() => setAnimated(!animated)}
+                     className={cn(styles.smile, {
+                         [styles.isAnimated]: animated
+                     })}/>
+            </div>
         </div>
-    )
-}
+    );
+};
